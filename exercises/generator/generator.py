8bc128e4be93bfa94901bc7523e1b4dc2033299b
Philipp WSL <johnyten@hotmail.com>
2023-04-02 14:41:33 +0200

    ads generator and magic method first exercises

--- exercises/generator/generator.py
+++ exercises/generator/generator.py
@@ -1,1 +1,22 @@
-# Philipp
+# Generator Function example
+import sys
+
+# normal function
+def count_Numbers(): 
+  
+    #some complex logic, used in a reaaaaalllyy big data set
+
+    print('results from each data point')
+
+
+
+
+
+ 
+# List comprehension  
+nums_squared_list = [i ** 2 for i in range(100)]  
+print('Memory in Bytes',sys.getsizeof(nums_squared_list))
+
+# Generator Expression  Syntax
+nums_squared_gc = (i ** 2 for i in range(100))  ##Creates a Generator object that can be iterated
+print('Memory in Bytes', sys.getsizeof( nums_squared_gc))  
--- exercises/magic-methods/magic-methods.py
+++ exercises/magic-methods/magic-methods.py
@@ -1,1 +1,12 @@
-# Philipp
+
+
+class A(object):
+    def __new__(cls):
+         print("Creating instance")
+         return super(A, cls).__new__(cls)
+  
+    def __init__(self):
+        print("Init is called")
+  
+A()
+print(A())
