8d2317cf0e9c0e2d13f8354ee03d41aa21f2f44f
Off3line <johnyten@hotmail.com>
2023-04-04 08:18:04 +0200

    ads magic methods examples

---
+++ exercises/magic-methods/Car.py
@@ -0,0 +1,29 @@
+
+
+class Car:
+    ## Same behavior as the new keyword in other languages like Java
+    ## Gets called first when the class is instantiated
+    ## returns the newly instantiated object
+    def __new__(cls):
+        print ("__new__ magic method is called")
+        inst = object.__new__(cls)
+        return inst
+    
+    ## Afterwards, init is called
+    ## Usually, you can define what happens with the passed arguments when the object is created here
+    def __init__(self):
+        print ("__init__ magic method is called")
+        self.name='Skoda'
+
+    ## Defines how a string should represent the object.
+    def __str__(self):
+        return 'Car name ' + self.name
+       
+
+
+
+car1 = Car()
+print(dir(car1)) ##Shows how many magic methods are inherited
+print(car1)
+
+print('Car name Skoda')
