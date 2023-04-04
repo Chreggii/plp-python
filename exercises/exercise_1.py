bb4831c48950c9f7b68f8afd5cc0b52e2ba99614
Philipp WSL <johnyten@hotmail.com>
2023-04-04 20:56:50 +0200

    ads the dscription

---
+++ exercises/exercise_1.py
@@ -0,0 +1,16 @@
+ ## Small Exercise
+
+
+## Define a class called book
+## everytime it is instantiated, it prints a comment like: 'Hi There!  I am a Book!' 
+## The class definition should also take a number as an argument which shows the number of pages and a string for the title
+## Define three methods called open, read, close  which print "Open/Read/Close the Book {name of book}"
+## Everytime you console print the object, the printed line should contain the book title and the number of pages
+## Implement a class named BookReader which can be used as a context manager. In the __enter__ method should open the book and the __exit__ method should close the book.
+
+## Once you are done, run the code below and check if the book are properly opened and closed.
+
+# book = Book(500, 'Harry Potter')
+# with BookReader(book) as book:
+#     book.read()
+
